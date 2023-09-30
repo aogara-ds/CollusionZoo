93b3d88befcae41019c597f53475c4a0d73c4b71
Aidan O'Gara <aidanogara623@gmail.com>
2023-09-30 14:52:50 -0700

    testing OddOneOut

---
+++ test.py
@@ -0,0 +1,13 @@
+from pettingzoo.sisl.pursuit import pursuit
+env = pursuit.env(render_mode="human", max_cycles=50)
+
+print(env)
+
+env.reset()
+for agent in env.agent_iter():
+    observation, reward, termination, truncation, info = env.last()
+    action = None if termination or truncation else env.action_space(agent).sample()  # this is where you would insert your policy
+    print(agent, action)
+    env.step(action)
+
+env.close()
